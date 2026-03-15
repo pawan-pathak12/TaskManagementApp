3d9d47ed7d588af646d867467f9b99fadc353ec7
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2026-03-15 18:48:42 +0545

    test: add API, data, and unit test projects
    - Basic setup still pending
    - Added TaskInMemoryRepo with IUserRepository implementation

--- TaskManagmentSystem.API/Entities/Task.cs
+++ TaskManagmentSystem.API/Entities/Task.cs
@@ -10,7 +10,7 @@ namespace TaskManagmentSystem.API.Entities
         public User User { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public TasksStatus Status { get; set; } = TasksStatus.Pending;
+        public TasksStatus Status { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public DateTimeOffset UpdatedAt { get; set; }
         public bool IsActive { get; set; } = true;
